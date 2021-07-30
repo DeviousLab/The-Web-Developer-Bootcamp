e486778e8c490076f2084851db77491efc2bfcd7
DeviousLab <deviouslab@gmail.com>
2021-07-30 19:04:23 +0400

    Completed TV Show Search
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- JavaScript/TV Show Search/app.js
+++ JavaScript/TV Show Search/app.js
@@ -1,6 +1,19 @@
 const form = document.querySelector('#searchForm');
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', async (e) => {
     e.preventDefault();
     const searchInput = form.elements.query.value;
-    
+    const config = { params: {q: searchInput} };
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+    showImages(res.data);
+    form.elements.query.value = '';
 })
+
+const showImages = (shows) => {
+    for(let result of shows){
+        if (result.show.image) {
+            const showCover = document.createElement('img');
+            showCover.src = result.show.image.medium;
+            document.body.append(showCover);
+        }
+    }
+}
