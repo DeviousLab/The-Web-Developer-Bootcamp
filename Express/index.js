36c94a26bee43f01db0a6362becf7613bfc714c0
DeviousLab <deviouslab@gmail.com>
2021-08-16 21:59:23 +0400

    Added default category selector
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- Express/index.js
+++ Express/index.js
@@ -18,8 +18,11 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
     
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+
+const categories = ['fruit', 'vegetables', 'dairy'];
 
 app.get('/products', async (req, res) => {
     const products = await Product.find({});
@@ -27,7 +30,7 @@ app.get('/products', async (req, res) => {
 });
 
 app.get('/products/new', (req, res) => {
-    res.render('products/new');
+    res.render('products/new', { categories });
 });
 
 app.post('/products', async (req, res) => {
@@ -46,7 +49,7 @@ app.get('/products/:id', async (req, res) => {
 app.get('/products/:id/edit', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
-    res.render('products/edit', { product });
+    res.render('products/edit', { product, categories });
 });
 
 app.put('/products/:id', async (req, res) => {
