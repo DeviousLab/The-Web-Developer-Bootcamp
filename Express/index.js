f2e9a71aa10bd487db4cb6baf0465f3e89716272
DeviousLab <deviouslab@gmail.com>
2021-08-16 18:25:11 +0400

    Created product details page with routing
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- Express/index.js
+++ Express/index.js
@@ -7,20 +7,28 @@ const Product = require('./models/product');
 
 mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-        console.log("MONGO CONNECTION OPEN!!!")
+        console.log("MONGO CONNECTION OPEN!!!");
     })
     .catch(err => {
-        console.log("OH NO MONGO CONNECTION ERROR!!!!")
-        console.log(err)
+        console.log("OH NO MONGO CONNECTION ERROR!!!!");
+        console.log(err);
     })
     
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/dog', (req, res) => {
-    res.send('woof');
+app.get('/products', async (req, res) => {
+    const products = await Product.find({});
+    res.render('products/index', { products })
+});
+
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    console.log(product);
+    res.render('products/show', { product })
 });
 
 app.listen(3000, () => {
-    console.log('Example app listening on port 3000!')
+    console.log('Example app listening on port 3000!');
 });
