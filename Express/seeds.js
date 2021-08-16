a1c2c9e42229e1501106b72f54231c639d65da70
DeviousLab <deviouslab@gmail.com>
2021-08-16 17:25:54 +0400

    Added sample product data

---
+++ Express/seeds.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Product = require('./models/product');
+
+mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("OH NO MONGO CONNECTION ERROR!!!!")
+        console.log(err)
+    })
+    
+const p = new Product({
+    name: 'Grapefruit',
+    price: '1.00',
+    category: 'fruit',
+});
+p.save().then(p => {
+    console.log(p);
+})
+    .catch(e => {
+        console.log(e)
+    });
