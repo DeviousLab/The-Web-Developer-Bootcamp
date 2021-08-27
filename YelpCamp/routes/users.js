51893928b87704036d1bd2f5605395ec44e88130
DeviousLab <deviouslab@gmail.com>
2021-08-28 03:47:48 +0400

    Register route logic done
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- YelpCamp/routes/users.js
+++ YelpCamp/routes/users.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
-router.post('/register', async (req, res) => {
-    res.send(req.body);
-});
+router.post('/register', catchAsync(async (req, res) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        req.flash('success', `Welcome to YelpCamp ${registeredUser.username}!`);
+        res.redirect('/campgrounds');
+    } catch (err) {
+        req.flash('error', err.message);
+        res.redirect('/users/register');
+    }
+}));
 
 module.exports = router;
