6cd42dd43ad30851213c341adeae7b411d6f7f6a
DeviousLab <deviouslab@gmail.com>
2021-08-28 03:56:54 +0400

    Created login form and routes
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- YelpCamp/routes/users.js
+++ YelpCamp/routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
+const passport = require('passport');
 const User = require('../models/user');
 
 router.get('/register', (req, res) => {
@@ -19,5 +20,14 @@ router.post('/register', catchAsync(async (req, res) => {
         res.redirect('/users/register');
     }
 }));
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login'}), (req, res) => {
+    req.flash('success', `Welcome back ${req.user.username}!`);
+    res.redirect('/campgrounds');
+});
 
 module.exports = router;
