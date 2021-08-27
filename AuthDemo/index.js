f692093d504690ca9009d60577efc5d2eaaa0660
DeviousLab <deviouslab@gmail.com>
2021-08-28 01:18:21 +0400

    Added bcrypt hashing for password
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- AuthDemo/index.js
+++ AuthDemo/index.js
@@ -1,14 +1,41 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const User = require('./models/user');
+const bcrypt = require('bcrypt');
+
+mongoose.connect('mongodb://localhost:27017/authDemo', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("OH NO MONGO CONNECTION ERROR!!!!")
+        console.log(err)
+    })
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send("Hello World!")
+});
 
 app.get('/register', (req, res) => {
     res.render('register');
 });
 
+app.post('/register', async(req, res) => {
+    const { password, username } = req.body;
+    const hash = await bcrypt.hash(password, 12);
+    const user = new User({
+        username,
+        password: hash
+    });
+    await user.save();
+    res.redirect('/'); 
+});
 app.get('/secret', (req, res) => {
     res.send('Secret');
 });
