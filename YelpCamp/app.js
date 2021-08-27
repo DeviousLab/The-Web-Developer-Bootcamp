9a3ce7f39d5dea9b4fc5a07c897af1f64bbef15f
DeviousLab <deviouslab@gmail.com>
2021-08-28 03:40:22 +0400

    Created register form
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -12,6 +12,7 @@ const User = require('./models/user');
 
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
+const userRoutes = require('./routes/users');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 	useNewUrlParser: true,
@@ -62,6 +63,7 @@ app.use((req, res, next) => {
 	next();
 });
 
+app.use('/', userRoutes);
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/reviews', reviewRoutes);
 
