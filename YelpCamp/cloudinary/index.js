bfaefb78f9779d273a6b21723bb12b5b8b8c4cb3
DeviousLab <deviouslab@gmail.com>
2021-08-31 03:19:09 +0400

    Changed to proper documentation param
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- YelpCamp/cloudinary/index.js
+++ YelpCamp/cloudinary/index.js
@@ -11,7 +11,7 @@ const storage = new CloudinaryStorage({
     cloudinary,
     params: {
         folder: 'YelpCamp',
-        allowedFormats: ['jpg', 'png', 'jpeg']
+        allowed_formats: ['jpg', 'png', 'jpeg']
     }
 });
 
