cde7964d763be45d256bc45350671425d3162f38
DeviousLab <deviouslab@gmail.com>
2021-08-31 05:27:00 +0400

    Centered marker on campground location
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- YelpCamp/public/javascripts/showMap.js
+++ YelpCamp/public/javascripts/showMap.js
@@ -2,6 +2,10 @@
     const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
+    center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 9 // starting zoom
     });
+
+const marker1 = new mapboxgl.Marker({ color: 'black', rotation: 45 })
+.setLngLat(campground.geometry.coordinates)
+.addTo(map);
