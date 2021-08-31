d0b26d546d51b2b90934f48cebcbe358d0db4ea1
DeviousLab <deviouslab@gmail.com>
2021-09-01 02:57:55 +0400

    Added engine and port checks
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -160,7 +160,11 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render('error', { err });
 });
 
-const port = process.env.PORT || 3000;
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
 app.listen(port, () => {
   console.log(`Server started on ${port}`);
 });
